fa2702ff1bb59302433977730cb24e857983a7bb
CKYKA513 <90738854+CKYKA513@users.noreply.github.com>
2022-08-11 14:53:38 -0400

    avencement

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 let menuBtn = document.querySelectorAll('.menu')
+let sections = document.querySelectorAll('main > div')
 
 menuBtn.forEach(btn => {
     btn.addEventListener('mouseenter', () => {
@@ -6,5 +7,16 @@ menuBtn.forEach(btn => {
     })
     btn.addEventListener('mouseout', () => {
         btn.classList.remove('menu_mouse_on')
+    })
+
+    btn.addEventListener('click', () => {
+        btn.textContent
+        let selectedSection = document.querySelector(`.${btn.textContent}`)
+        sections.forEach(section => {
+            section.style.display = 'none'
+            if (section.classList.contains(`${btn.textContent}`) == true) {
+                selectedSection.style.display = 'block'
+            }
+        })
     })
 })
