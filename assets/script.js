d160fd8e096dd9eda839d738472a0142246c8802
CKYKA513 <90738854+CKYKA513@users.noreply.github.com>
2022-10-09 11:43:16 -0400

    stuff

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,21 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+let panels = gsap.utils.toArray(".panel");
+
+gsap.to(panels, {
+  xPercent: -100 * (panels.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (panels.length - 1),
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: "+=3500",
+  }
+});
+
 let menuBtn = document.querySelectorAll('.menu')
 let sections = document.querySelectorAll('main > div')
 let btnPfl = document.querySelectorAll('.btn')
@@ -10,21 +28,21 @@ menuBtn.forEach(btn => {
         btn.classList.remove('menu_mouse_on')
     })
 
-    btn.addEventListener('click', () => {
-        btn.textContent
-        let selectedSection = document.querySelector(`.${btn.textContent}`)
-        sections.forEach(section => {
-            section.style.display = 'none'
-            if (section.classList.contains(`${btn.textContent}`) == true) {
-                selectedSection.style.display = 'block'
-            }
-        })
-    })
+    // btn.addEventListener('click', () => {
+    //     btn.textContent
+    //     let selectedSection = document.querySelector(`.${btn.textContent}`)
+    //     sections.forEach(section => {
+    //         section.style.display = 'none'
+    //         if (section.classList.contains(`${btn.textContent}`) == true) {
+    //             selectedSection.style.display = 'block'
+    //         }
+    //     })
+    // })
 })
 
-btnPfl.forEach(btn => {
-    btn.addEventListener('click', () => {
-        let selectedSection = document.querySelector(`.${btn.textContent}`)
+// btnPfl.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         let selectedSection = document.querySelector(`.${btn.textContent}`)
         
-    })
-})
+//     })
+// })
